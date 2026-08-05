8edc4c5f6d3c6c92aee90d6449eec74fd71f0d28
emrebicen-svg <bicenemre31@gmail.com>
2026-08-05 13:10:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -283,3 +283,10 @@ window.addEventListener("load", () => {
     });
 
 });
+document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+});
+
+document.addEventListener("dragstart", (e) => {
+    e.preventDefault();
+});
